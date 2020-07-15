c570ff733d81bcb770341457f836b8eeb774d09d
Felipe Ribeiro <felipe.engcontrole@gmail.com>
2020-07-15 10:55:44 -0300

    Create tipo_none.py

---
+++ week1/day3/tipo_none.py
@@ -0,0 +1,27 @@
+"""
+None
+
+-> É sempre considerado como FALSE
+
+"""
+print("\n")
+
+#Tipo None -> 'tipo sem tipo' ou 'tipo vazio'
+
+sem_tipo = None #Utilizamos quando queremos criar uma variável e inicializá-la com um tipo sem tipo antes...
+#... de recebermos um valor final
+
+#Ou seja, posteriormente essa variável pode receber qualquer tipo de dado.
+
+print(sem_tipo)
+print(type(sem_tipo))
+#=================================================================================================================
+print("\n")
+# -> É sempre considerado como FALSE
+
+if None:
+    print(True)
+else:
+    print(False) # O None sempre será False
+
+#=================================================================================================================
