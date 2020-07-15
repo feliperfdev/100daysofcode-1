c06eeaf08edb264ab1c1bafef1e3f14cf4f5c374
Felipe Ribeiro <felipe.engcontrole@gmail.com>
2020-07-15 11:16:36 -0300

    Update dicionarios.py

--- week1/day3/dicionarios.py
+++ week1/day3/dicionarios.py
@@ -37,3 +37,41 @@ print(paises.get('br'))
 print(countries.get('nao_existe')) #Não dá ero, ao invés disso, retorna o tipo None
 
 print("\n")
+#=================================================================================================================
+
+paises = {'br': 'Brasil', 'eua': 'United States', 'jp': 'Japão'}
+
+russia = paises.get('ru') # None --> False
+japao = paises.get('jp')
+
+if japao:
+    print(f'Encontrei o país {japao}')
+else:
+    print(f'Não encontrei o país')
+
+print("\n")
+
+if russia:
+    print(f'Encontrei o país {russia}')
+else:
+    print(f'Não encontrei o país')
+
+#=================================================================================================================
+print("\n")
+
+paises = {'br': 'Brasil', 'eua': 'United States', 'jp': 'Japão', 'kor': 'Coreia'}
+
+coreia = paises.get('kor', 'Não encontrado')
+#O que significa: Se encontrar a chave 'kor', ele vai retornar o valor. Caso não encontre, ...
+#... ele vai retornar o 'Não encontrado'
+
+#Assim não precisaremos mais do condicional if
+
+print(f'{coreia}')
+
+print("\n")
+paises_1 = {'br': 'Brasil', 'eua': 'United States', 'jp': 'Japão'}
+
+coreia_1 = paises_1.get('kor', 'Não encontrado')
+
+print(f'{coreia_1}')
