f9abac5386ca59aaa9771f4310b5b3af5732be74
Felipe Ribeiro <felipe.engcontrole@gmail.com>
2020-07-15 10:58:32 -0300

    Update dicionarios.py

--- week1/day3/dicionarios.py
+++ week1/day3/dicionarios.py
@@ -37,12 +37,3 @@ print(paises.get('br'))
 print(countries.get('nao_existe')) #Não dá ero, ao invés disso, retorna o tipo None
 
 print("\n")
-#Tipo None -> 'tipo sem tipo' ou 'tipo vazio'
-
-sem_tipo = None #Utilizamos quando queremos criar uma variável e inicializá-la com um tipo sem tipo antes...
-#... de recebermos um valor final
-
-#Ou seja, posteriormente essa variável pode receber qualquer tipo de dado.
-
-print(sem_tipo)
-print(type(sem_tipo))
