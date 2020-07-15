9ea5ad563b42db2d5a671e22e1c8e829e8c5b8ac
Felipe Ribeiro <felipe.engcontrole@gmail.com>
2020-07-15 10:55:52 -0300

    Update dicionarios.py

--- week1/day3/dicionarios.py
+++ week1/day3/dicionarios.py
@@ -1,9 +1,48 @@
 """"
-Dicionários (dict)
+Dictionaries (dict)
 
--> Em outras línguagens, são conhecidos como 'mapas'
+-> Em outras linguagens os dicionários podem ser chamados de 'mapas'
 
--> Dicionários são coleções do tipo chave/valor
+-> Dicionários são coleções do tipo chave/valor --> {chave:valor}
 -> São representados por chaves {}
+-> Tanto chaves quanto os valores podem ser de quaisquer tipos de dados
+-> Dicionários não são indexados
 
 """
+print("\n")
+
+#Formar de representação
+
+# -> Mais comum:
+paises = {'br': 'Brasil', 'eua': 'United States'}
+print(type(paises))
+
+# -> Menos comum:
+
+countries = dict(br='Brasil', eua='United States')
+print(type(countries))
+
+#=================================================================================================================
+print("\n")
+# Acessando elementos:
+
+print(paises['br']) #Acessamos os valores através das chaves
+print(paises['eua'])
+#print(countries['nao_existe']) #Erro
+
+print("\n")
+# Acessando elementos utilizando o get(): --> Recomendado!!!
+
+print(paises.get('br'))
+print(countries.get('nao_existe')) #Não dá ero, ao invés disso, retorna o tipo None
+
+print("\n")
+#Tipo None -> 'tipo sem tipo' ou 'tipo vazio'
+
+sem_tipo = None #Utilizamos quando queremos criar uma variável e inicializá-la com um tipo sem tipo antes...
+#... de recebermos um valor final
+
+#Ou seja, posteriormente essa variável pode receber qualquer tipo de dado.
+
+print(sem_tipo)
+print(type(sem_tipo))
