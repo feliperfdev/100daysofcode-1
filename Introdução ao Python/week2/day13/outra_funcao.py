980503e0b6d74f2b6f458b2dc9b4511ead58e6ab
Felipe Ribeiro <felipe.engcontrole@gmail.com>
2020-07-25 16:37:45 -0300

    Python

--- Introdução ao Python/week2/day13/outra_funcao.py
+++
@@ -1,8 +0,0 @@
-def qualquerFuncao():
-    return 'Felipe Ribeiro'
-
-if __name__ == "__main__":
-    qualquerFuncao()
-    print('outra_funcao.py foi importado')
-else:
-    print('importado')
--- Introdução ao Python/week2/day13/quadratica.py
+++
@@ -1,3 +0,0 @@
-def geraFuncaoQuadratica(a, b, c):
-    "Gera uma função do tipo ax²+bx+c"
-    return lambda x: a*x**2 + b*x + c
