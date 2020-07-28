79c89aacdd070461b0c24d190b263381449493d6
Felipe Ribeiro <felipe.engcontrole@gmail.com>
2020-07-27 22:49:05 -0300

    Python

---
+++ Introdução ao Python/week3/day15/criarLista.py
@@ -0,0 +1,26 @@
+from time import sleep
+
+print('--------------------Lista--------------------')
+nome_lista = input('Digite o nome da lista: ')
+print(f'--------------------{nome_lista}--------------------')
+
+with open(f'{nome_lista}.txt', 'w') as arquivo:
+    arquivo.write(f'--------------------{nome_lista}--------------------\n\n')
+    escolha = ()
+
+    try:
+        while(escolha != 2):
+            print('O que deseja fazer? ')
+            escolha = int(input('\n[1] - Adicionar elemento na lista\n[2] - Sair da lista\n'))
+            if (escolha == 1):
+                elemento = int(input('Quantidade de elementos: '))
+                for e in range(1, elemento+1):
+                    elemento = input('')
+                    arquivo.write(f'{elemento}\n')
+            if (escolha == 2):
+                    arquivo.close()
+    except Exception as error:
+        print(f'\nOpa! Aparentemente, ocorreu um erro. {error.__class__}')
+    finally:
+        print('\nObrigado por usar a lista.')
+    sleep(1.5)
--- Introdução ao Python/week3/day15/escrevendo_arquivos.py
+++ Introdução ao Python/week3/day15/escrevendo_arquivos.py
@@ -1,5 +1,22 @@
 '''
 Escrevendo em arquivos
 
+
+OBS: Ao abrir um arquivo para LEITURA, não podemos realizar escrita nele, apenas ler.
+Da mesma forma, se abrirmos um arquivo para ESCRITA, não podemos lê-lo (no terminal), apenas escrever.
+
+Quando é criado um arquivo para escrita, ele é criado no sistema operacional com o nome escolhido
+e com o que foi feito/escrito nele.
+
+-> Abrindo um arquivo para escrita com o modo 'w', se o arquivo não existir, será criado. Caso já exista,
+o anterior será apagado e um novo será criado. Dessa forma, todo o conteúdo do arquivo anterior é perdido.
+
 '''
 print('\n')
+
+with open('novo_arquivo.txt', 'w') as arquivo: # o 'w' vem de 'write'
+    arquivo.write('Posso escrever qualquer coisa aqui.')
+    arquivo.write('\nIsso é apenas um teste')
+    arquivo.write('\nÚltima linha desse teste')
+
+# --> Função write() serve para escrever e recebe APENAS strings
